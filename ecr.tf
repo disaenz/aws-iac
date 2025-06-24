10dba1b5ec204db295f8d8911c961f3c136864f1
Daniel Saenz <disaenz2@gmail.com>
2025-06-24 13:15:29 -0600

    Adding Public ECR to AWS to store all our portfolio services

---
+++ ecr.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_ecrpublic_repository" "services" {
+  repository_name      = var.ecr_repository_name
+  image_tag_mutability = "MUTABLE"
+
+  catalog_data {
+    description = "Container images for portfolio services"
+  }
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Environment = "production"
+    Project     = "portfolio"
+  }
+}
+
+output "ecr_public_repository_url" {
+  description = "Public ECR repository URI"
+  value       = aws_ecrpublic_repository.services.repository_url
+}
