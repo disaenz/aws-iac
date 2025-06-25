7e4de02ea9c4ab077e7f01b5c8ef8cd0e274d352
Daniel Saenz <disaenz2@gmail.com>
2025-06-25 13:57:17 -0600

    Adding iam policies to lambda and ecr to pull images

--- iam.tf
+++ iam.tf
@@ -14,3 +14,8 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_iam_role_policy_attachment" "lambda_ecr" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
