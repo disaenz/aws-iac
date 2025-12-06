48fd05d2517684ce826650541da8f7015336810c
Daniel Saenz <disaenz2@gmail.com>
2025-12-05 21:53:06 -0700

    infra: increase portfolio-api Lambda to 512MB + 30s timeout to prevent OpenAI request timeouts

--- lambda.tf
+++ lambda.tf
@@ -12,8 +12,8 @@ resource "aws_lambda_function" "portfolio_api" {
   image_uri     = "${data.aws_ecr_repository.portfolio_api.repository_url}:latest"
   role          = aws_iam_role.lambda_exec.arn
 
-  memory_size   = 128
-  timeout       = 10
+  memory_size   = 512
+  timeout       = 30
 
   environment {
     variables = {
