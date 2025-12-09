6cf837de41a9d61c1aa87bb020daede51c652db6
Daniel Saenz <disaenz2@gmail.com>
2025-12-09 14:32:13 -0700

    feat: enable SES sending support for portfolio API Lambda

---
+++ ses.tf
@@ -0,0 +1,78 @@
+########################################
+# SES Domain Identity (must match Route 53 domain)
+########################################
+resource "aws_ses_domain_identity" "portfolio" {
+  domain = var.domain_name
+}
+
+########################################
+# SES DKIM Signing - improves deliverability & trust
+########################################
+resource "aws_ses_domain_dkim" "portfolio" {
+  domain = aws_ses_domain_identity.portfolio.domain
+}
+
+########################################
+# TXT Record for SES Domain Verification
+########################################
+resource "aws_route53_record" "ses_verification" {
+  zone_id = var.zone_id
+  name    = "_amazonses.${var.domain_name}"
+  type    = "TXT"
+  ttl     = 180
+  records = [aws_ses_domain_identity.portfolio.verification_token]
+}
+
+########################################
+# SES DKIM CNAME Records
+########################################
+resource "aws_route53_record" "ses_dkim" {
+  count   = 3
+  zone_id = var.zone_id
+  name    = "${aws_ses_domain_dkim.portfolio.dkim_tokens[count.index]}._domainkey.${var.domain_name}"
+  type    = "CNAME"
+  ttl     = 180
+  records = ["${aws_ses_domain_dkim.portfolio.dkim_tokens[count.index]}.dkim.amazonses.com"]
+}
+
+########################################
+# Verified Sender Email Identity
+########################################
+resource "aws_ses_email_identity" "noreply" {
+  email = "noreply@${var.domain_name}"
+}
+
+########################################
+# IAM Policy for Portfolio API Email Send Permissions
+########################################
+resource "aws_iam_policy" "ses_email_policy" {
+  name        = "portfolio-api-ses-policy"
+  description = "Allow Lambda to send emails via SES"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "ses:SendEmail",
+          "ses:SendRawEmail"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+########################################
+# Outputs
+########################################
+output "ses_verified_email" {
+  description = "The verified noreply email identity"
+  value       = aws_ses_email_identity.noreply.email
+}
+
+output "ses_domain_arn" {
+  description = "SES Domain Identity ARN"
+  value       = aws_ses_domain_identity.portfolio.arn
+}
