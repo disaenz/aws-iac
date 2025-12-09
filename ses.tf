573bb3ddfa26355818a3affbd15cd8a2871c52c3
Daniel Saenz <disaenz2@gmail.com>
2025-12-09 14:43:45 -0700

    fixing zone_id

--- ses.tf
+++ ses.tf
@@ -1,5 +1,5 @@
 ########################################
-# SES Domain Identity (must match Route 53 domain)
+# SES Domain Identity (same as Route 53 domain)
 ########################################
 resource "aws_ses_domain_identity" "portfolio" {
   domain = var.domain_name
@@ -16,23 +16,25 @@ resource "aws_ses_domain_dkim" "portfolio" {
 # TXT Record for SES Domain Verification
 ########################################
 resource "aws_route53_record" "ses_verification" {
-  zone_id = var.zone_id
+  zone_id = data.aws_route53_zone.primary.zone_id
   name    = "_amazonses.${var.domain_name}"
   type    = "TXT"
-  ttl     = 180
+  ttl     = 300
   records = [aws_ses_domain_identity.portfolio.verification_token]
 }
 
 ########################################
-# SES DKIM CNAME Records
+# SES DKIM CNAME Records (3 required)
 ########################################
 resource "aws_route53_record" "ses_dkim" {
   count   = 3
-  zone_id = var.zone_id
+  zone_id = data.aws_route53_zone.primary.zone_id
   name    = "${aws_ses_domain_dkim.portfolio.dkim_tokens[count.index]}._domainkey.${var.domain_name}"
   type    = "CNAME"
-  ttl     = 180
-  records = ["${aws_ses_domain_dkim.portfolio.dkim_tokens[count.index]}.dkim.amazonses.com"]
+  ttl     = 300
+  records = [
+    "${aws_ses_domain_dkim.portfolio.dkim_tokens[count.index]}.dkim.amazonses.com"
+  ]
 }
 
 ########################################
@@ -43,7 +45,7 @@ resource "aws_ses_email_identity" "noreply" {
 }
 
 ########################################
-# IAM Policy for Portfolio API Email Send Permissions
+# IAM Policy for Lambda Email Send Permissions
 ########################################
 resource "aws_iam_policy" "ses_email_policy" {
   name        = "portfolio-api-ses-policy"
@@ -53,8 +55,8 @@ resource "aws_iam_policy" "ses_email_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Effect   = "Allow",
-        Action   = [
+        Effect = "Allow",
+        Action = [
           "ses:SendEmail",
           "ses:SendRawEmail"
         ],
